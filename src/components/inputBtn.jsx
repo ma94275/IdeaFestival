1ad068f1239db3ce6ced1f43f663ba9ba5ecbbdf
ma94275 <s25020@gsm.hs.kr>
2025-12-19 08:55:44 +0900

    chore: 컴포넌트 간격 수정

--- src/components/inputBtn.jsx
+++ src/components/inputBtn.jsx
@@ -2,7 +2,7 @@ export default function InputBtn({label, onClick}){
     return(
         <div>
             <button 
-                className="w-[360px] h-[45px] bg-[#002455] text-white rounded-[10px] font-pretendad font-semibold"
+                className="w-[360px] h-[45px] bg-[#002455] text-white text-[20px] rounded-[10px] font-pretendad font-semibold"
                 onClick={onClick}>{label}
             </button>
         </div>
